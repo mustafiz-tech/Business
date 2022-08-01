e9a773082af4fbddd5e063504b6d41786d9d013c
mustafiz-tech <102187767+mustafiz-tech@users.noreply.github.com>
2022-08-01 20:05:15 +0530

    change

--- js/myjava.js
+++ js/myjava.js
@@ -21,13 +21,16 @@ $(function(){
     $('#close').click(function(){
         $('#slideOpen').removeClass('slideEffect');
      });
-})
 
+
+
+     
 // ----------  active class  -----------
 
-$(function(){
-    $('li a').click(function(){
-        $('li a').removeClass('active');
-        $(this).addClass('avtive')
-    });
-});
+    $("nav-ul li a").click(function(){
+        $(".nav-ul li a").removeClass("active")
+        $(this).addClass("active");
+    })
+
+})
+
